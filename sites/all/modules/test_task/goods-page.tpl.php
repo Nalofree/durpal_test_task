299215f708e75a0b3e1a90349f68febeaf752629
nalofree <nalofree@gmail.com>
2018-03-19 01:29:03 +0800

    install dpl and back module part

--- sites/all/modules/test_task/goods-page.tpl.php
+++ sites/all/modules/test_task/goods-page.tpl.php
@@ -1,8 +1,9 @@
 <div id="goods-page">
     <input type="text" name="search">
-    <?php foreach ($goods as $good): ?>
-        <span>
-            <?php echo $good->name; ?>
-        </span>
-    <?php endforeach; ?>
+    <p><?php echo $name; ?></p>
+<!--    --><?php //foreach ((array) $goods as $good): ?>
+<!--        <span>-->
+<!--            --><?php //echo $good->name; ?>
+<!--        </span>-->
+<!--    --><?php //endforeach; ?>
 </div>
